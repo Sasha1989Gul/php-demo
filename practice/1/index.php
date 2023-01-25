36a4ef7b78b0d61175b30924979ce3fd084e0110
Sasha1989Gul <118977314+Sasha1989Gul@users.noreply.github.com>
2023-01-25 16:54:18 +0000

    practice

--- practice/1/index.php
+++ practice/1/index.php
@@ -1,58 +1,91 @@
+<style>
+    b {
+        color: red;
+    }
+</style>
+
+<p><b>1. Вывести на страницу «Hello World» (echo и print).</b></p>
 <?php 
-//1. Вывести на страницу «Hello World» (echo и print).
 $string1 = "Hello";
 $string2 = "World!";
 
-echo "$string1 $string2";
-
-echo "<br>";
-
+echo "$string1 $string2" . "<br>";
 print "{$string1} {$string2}";
+?>
 
-echo "</br>";
-//2. ESCAPE последовательности: вывести «Hello World» в различных вариациях:
-     //a) «Hello» в первой строке, «World» во второй;
+<p><b>2. ESCAPE последовательности: вывести «Hello World» в различных вариациях:<br></b></p>
+     <p><b>a) «Hello» в первой строке, «World» во второй;</b></p>
+<?php     
      echo $string1 . "</br>" .  $string2;
-echo "</br>";
-     //b) «Hello» в первой строке, «World» во второй и с табуляцией;
-     echo $string1 . "</br>" . " " . $string2;
-echo "</br>";
-     //c) Вывести на страницу строку: «Hello World. \”It’s my page!”\»;
-     echo "Hello World. \"It's my page!\"";
-echo "</br>";
-     //d)Вывести на страницу строку: <h1>Hello</h1> <h3> World</h3> <b>I</b> <i>am</i> <u>a new PHP developer</u>;
-     echo "<h1>Hello</h1> <h3> World</h3> <b>I</b> <i>am</i> <u>a new PHP developer</u>";
-echo "</br>";
-     //e) Вывести на страницу строку: <script>alert(‘Hello World’); </script>.
-     echo "<script>alert(‘Hello World’); </script>";
-echo "<br>";
-//3. Вывести значение, противоположное введенному в переменную (input "5", output "-5").
-$i = 5;
-$s = $i * -1;
-echo $s;
+?>
 
-echo "<br>";
-//4. Создать две переменные и вывести результат возможных математических операций между ними (+, -, *, /,%).
+     <p><b>b) «Hello» в первой строке, «World» во второй и с табуляцией;</b></p>
+<?php     
+     echo $string1 . "</br>" . " " . $string2;
+?>
+
+     <p><b>c) Вывести на страницу строку: «Hello World. \”It’s my page!”\»;</b></p>
+<?php     
+     echo "Hello World. \"It's my page!\"";
+?>
+
+     <p><b>d)Вывести на страницу строку: <h1>Hello</h1> <h3> World</h3> <b>I</b> <i>am</i> <u>a new PHP developer</u>;</b></p>
+<?php     
+     echo "<h1>Hello</h1> <h3> World</h3> <b>I</b> <i>am</i> <u>a new PHP developer</u>";
+?>
+
+     <p><b>e) Вывести на страницу строку: &lt;script&gt;alert(‘Hello
+World’); &lt;/script&gt;</b></p>
+<?php     
+     echo "<script>alert(‘Hello World’); </script>";
+?>
+
+<p><b>3. Вывести значение, противоположное введенному в переменную (input "5", output "-5").</b></p>
+<?php
+$i = 4;
+echo "Значение переменной " . $i . ", противоположное значение " . -$i;
+?>
+
+<p><b>4. Создать две переменные и вывести результат возможных математических операций между ними (+, -, *, /,%).</b></p>
+<?php
 $number1 = 6;
 $number2 = 2;
-echo "'Сумма равна ' + ($number1 + $number2), 'Вычетание равно ' + ($number1 - $number2), 'Деление равно ' + ($number1 / $number2), 'Умножение равно ' + ($number1 * $number2)";
+echo "Первое число " . $number1 . ", второе число " . $number2 . "<br>";
+echo "Сумма равна " . $number1 + $number2 . "<br>";
+echo "Разность равна " . $number1 - $number2 . "<br>";
+echo "Произведение равно " . $number1 * $number2 . "<br>";
+echo "Частное равно " . $number1 / $number2 . "<br>";
+echo "Остаток от деления равен " . $number1 % $number2;
+?>
 
-echo "<br>";
-//5. Поменять два числа местами.
-$number = 49;
-$x = $number % 10;
-$y = Math.floor($number / 10);
-echo String($x) + String($y);
+<p><b>5. Поменять два числа местами.</b></p>
+<?php
+$number1 = 4;
+$number2 = 9;
+echo "первое число " . $number1 . ", второе число " . $number2 . "<br>";
+$number1 = $number1 + $number2;
+$number2 = $number1 - $number2;
+$number1 = $number1 - $number2;
+echo "B обратном порядке: первое число " . $number1 . ", второе число " . $number2;
+?>
 
-echo "<br>";
-//6. Создать две переменные: username / password, вывести на страницу два input-тега, типа text,
-// где placeholder — это название переменной, а text — значение, записанное в переменную.
+<p><b>6. Создать две переменные: username / password, вывести на страницу два input-тега, типа text,
+где placeholder — это название переменной, а text — значение, записанное в переменную.</b></p>
+<?php
+$username = 'User1';
+$password = 'pass1';
+echo '<label for="">Username: <input type="text" placeholder="' . $username . '"></label>';
+echo '<label for=""> Password: <input type="password" placeholder="' . $password . '"></label>';
 
-echo "<br>";
-//7. Создать страницу, где описать переменные: font_family, font_size, font_style. Значение в этих
-// переменных — это value соответствующего стиля. вывести тег <p> со стилями, описанными в переменных.
-$i = Arial;
-$b = 60;
-$c = italic;
-echo '<p style="font_family:' . value($i) . '; font-size:' . value($b) . 'px; font_style:' . value($c) . '"> Hello </p>'
+?>
+
+<p><b>7. Создать страницу, где описать переменные: font_family, font_size, font_style. Значение в этих
+переменных — это value соответствующего стиля. вывести тег &ltp&gt со стилями, описанными в переменных.</b></p>
+<?php
+
+$font_family = 'Arial';
+$font_size = '60px';
+$font_style = 'italic';
+echo '<p style="font-family:' . $font_family . '; font-size:' . $font_size . '; font-style:' . $font_style . '">Hello</p>';
+
 ?>
--- practice/4.1/index.php
+++ practice/4.1/index.php
@@ -1,30 +1,190 @@
+<style>
+    b {
+        color: red;
+    }
+</style>
+
+<p><b>1. Создать массив из 10 элементов (чисел). Вывести:<br>
+    a) четные элементы;<br>
+    b) максимальный и минимальный элемент;<br>
+    c) отсортировать массив по возрастанию.</b></p>
 <?php 
-//1. Создать массив из 10 элементов (чисел). Вывести:
-    //a) четные элементы;
-    //b) максимальный и минимальный элемент;
-    //c) отсортировать массив по возрастанию.
+$arr1 = [];
+for ($i = 0; $i < 10; $i++) {
+    $arr1[$i] = rand(1, 20);
+}
+echo '<h2> Input array: ' . implode(', ', $arr1) . '</h2>';
 
-echo "<br>";
-//2. Создать 2 массива с числами по 10 элементов. Создать скрипт, который объединит уникальные значения из двух
-// массивов в один. 
+$arrEven = [];
+$max = $arr1[0];
+$min = $arr1[0];
+foreach ($arr1 as $i => $value) {
+    if ($value % 2 == 0) {
+        $arrEven[] = $value;
+    }
+    if ($value > $max) {
+        $max = $value;
+    }
+    if ($value < $min) {
+        $min = $value;
+    }
+}
+echo '<h2> Even: ' . implode(',', $arrEven) . '</h2>';
 
-echo "<br>";
-//3. Создать массив из 10 элементов. Посчитать количество повторов каждого числа в массиве (цвет цифр, в которых
-// нет повторов — красный, цвет тех чисел, в которых есть повторы — синий). Размер текста — 20рх. Число в каждой
-// строке выделять жирным.
+echo '<h2>MAX: ' . $max . ', MIN: ' . $min . '</h2>';
 
-echo "<br>";
-//4. Создать массив фруктов (состоит из названия, типа, цены и количества). Проделать с массивом такие действия:
-    //d) вывести только цитрусовые;
-    //e) посчитать и вывести цену фруктов;
-    //f) посчитать общую цену всех фруктов.
+sort($arr1);
+echo '<h2> Sorted array: ' . implode(', ', $arr1) . '</h2>';
+?>
+<p><b>2. Создать 2 массива с числами по 10 элементов. Создать скрипт, который объединит уникальные значения из двух
+массивов в один.</b></p> 
+<?php
+$array1 = [];
+$array2 = [];
+$resultArray = [];
 
-echo "<br>";
-//5. Создать массив со странами и вывести <select>-элемент, который будет включать в себя список стран
+for ($i = 0; $i < 10; $i++) {
+    $array1[$i] = rand(1, 33);
+    $array2[$i] = rand(1, 33);
+}
+echo '<h2> Input 1 array: ' . implode(', ', $array1) . '</h2>';
+echo '<h2> Input 2 array: ' . implode(', ', $array2) . '</h2>';
 
-echo "<br>";
-//6. Создать массив, который будет содержать название изображения, ссылки и альтернативный текст на следующие
-// ресурсы: Youtube, Facebook, Google, Gmail и разработать <div>, который будет показывать изображение и по
-// клику на изображение будет перенаправлять на соответствующий сайт.
+$newArray = array_merge($array1, $array2);
 
+for ($i = 0; $i < count($newArray); $i++) {
+    $counter = 0;
+    for ($y = $i + 1; $y < count($newArray); $y++) {
+        if ($newArray[$i] == $newArray[$y]) {
+            $counter += 1;
+        }
+    }
+    if ($counter == 0) {
+        $resultArray[] = $newArray[$i];
+    }
+}
+echo '<h2> Output array: ' . implode(', ', $resultArray) . '</h2>';
+?>
+<p><b>3. Создать массив из 10 элементов. Посчитать количество повторов каждого числа в массиве (цвет цифр, в
+     которых нет повторов — красный, цвет тех чисел, в которых есть повторы — синий). Размер текста — 20рх.
+      Число в каждой строке выделять жирным.</b></p>
+<style>
+    .blue {
+        color: blue;
+        font-size: 20px;
+    }
+
+    .red {
+        color: red;
+        font-size: 20px;
+    }
+</style>
+<?php
+$arr = [];
+for ($i = 0; $i < 10; $i++) {
+    $arr[$i] = rand(0, 9);
+}
+echo '<h2> Input array: ' . implode(', ', $arr) . '</h2>';
+
+$result = array_count_values($arr);
+arsort($result);
+foreach ($result as $key => $value) {
+    if ($value > 1) {
+        echo '<p class="blue"><strong>' . $key . '</strong> - ' . $value . '</p>';
+    } else {
+        echo '<p class="red"><strong>' . $key . '</strong> - ' . $value . '</p>';
+    }
+}
+?>
+<p><b>4. Создать массив фруктов (состоит из названия, типа, цены и количества). Проделать с массивом такие
+     действия:<br>
+    d) вывести только цитрусовые;<br>
+    e) посчитать и вывести цену фруктов;<br>
+    f) посчитать общую цену всех фруктов.</b></p>
+<?php
+$fruits = array(
+    "Orange" => array('Type' => 'Citrus', 'Price' => rand(1, 50), 'Amount' => rand(1, 9),),
+    "Banana" => array('Type' => 'Fruit', 'Price' => rand(1, 50), 'Amount' => rand(1, 9)),
+    "Lemon"  => array('Type' => 'Citrus', 'Price' => rand(1, 50), 'Amount' => rand(1, 9)),
+    "Apple"  => array('Type' => 'Fruit', 'Price' => rand(1, 50), 'Amount' => rand(1, 9)),
+    "Kiwi"   => array('Type' => 'Fruit', 'Price' => rand(1, 50), 'Amount' => rand(1, 9))
+);
+echo '<h1 style="margin:2px;"> All </h1>';
+$total = 0;
+foreach ($fruits as $name => $items) {
+    echo '<div style="float:left; padding:10px; background-color:green;"><h2>' . $name . '</h2>';
+    $items['Total price'] = $items['Price'] * $items['Amount'];
+
+    foreach ($items as $key => $value) {
+        if ($key === 'Type') {
+            echo '<h5>' . $key . ' : ' . $value . '; ';
+        }
+        if ($key === 'Price') {
+            echo $key . ' : ' . $value . '</h5>';
+        }
+        if ($key === 'Amount') {
+            echo '<h3>' . $key . ' : ' . $value . '</h3>';
+        } else if ($key === 'Total price') {
+            echo '<h6>' . $key . ' : ' . $value . '</h6>';
+            $total += $value;
+        }
+    }
+    echo '</div>';
+}
+echo '<div style="clear:both;"></div>';
+echo '<h1 style="margin:2px;"> Citrus </h1>';
+
+foreach ($fruits as $name => $items) {
+    $items['Total price'] = $items['Price'] * $items['Amount'];
+    if ($items['Type'] === 'Citrus') {
+        echo '<div style="float:left; padding:10px; background-color:orange;"><h2>' . $name . '</h2>';
+        foreach ($items as $key => $value) {
+            if ($key === 'Type') {
+                echo '<h5>' . $key . ' : ' . $value . '; ';
+            }
+            if ($key === 'Price') {
+                echo $key . ' : ' . $value . '</h5>';
+            }
+            if ($key === 'Amount') {
+                echo '<h3>' . $key . ' : ' . $value . '</h3>';
+            } else if ($key === 'Total price') {
+                echo '<h6>' . $key . ' : ' . $value . '</h6>';
+            }
+        }
+        echo '</div>';
+    }
+}
+echo '<div style="clear:both;"></div>';
+echo '<h1 style="margin:2px;"> Total: ' . $total . '</h1>';
+?>
+<p><b>5. Создать массив со странами и вывести &lt;select&gt;-элемент, который будет включать в себя список стран.</b></p>
+<?php
+$data = file_get_contents("http://country.io/names.json");
+$countries = json_decode($data, true);
+sort($countries);
+
+echo '<select name="country">';
+foreach ($countries as $key => $country) {
+    echo '<option value="' . $key . '">' . $country . '</option>';
+}
+echo '</select>';
+?>
+<p><b>6. Создать массив, который будет содержать название изображения, ссылки и альтернативный текст на 
+    следующие ресурсы: Youtube, Facebook, Google, Gmail и разработать &lt;div&gt;, который будет показывать 
+        изображение и по клику на изображение будет перенаправлять на соответствующий сайт.</b></p>
+<style>
+    img{
+        width: 50px;
+    }
+</style>
+<?php
+$x = array(
+    "Youtube"  => array('name' => 'youtube', 'a' => 'https://www.youtube.com', 'img' => 'youtube.png',),
+    "Facebook" => array('name' => 'facebook', 'a' => 'https://www.facebook.com', 'img' => 'facebook.png'),
+    "Google"   => array('name' => 'google', 'a' => 'https://www.google.com', 'img' => 'google.png'),
+    "Gmail"    => array('name' => 'gmail', 'a' => 'https://www.gmail.com', 'img' => 'gmail.png')
+);
+foreach ($x as $name => $items) {
+    echo '<div style="float: left;"><a href="' . $items['a'] . '"><img src="img/' . $items['img'] . '" alt=""></a>';
+}
 ?>
